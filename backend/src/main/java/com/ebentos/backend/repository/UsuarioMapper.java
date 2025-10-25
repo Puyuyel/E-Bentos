82e649be0d416c92353cc62cab308cc91a274c46
Puyuyel <paparaya48@gmail.com>
2025-10-24 21:42:17 -0500

    Cambios mínimos de estilo

--- backend/src/main/java/com/ebentos/backend/repository/UsuarioMapper.java
+++ backend/src/main/java/com/ebentos/backend/repository/UsuarioMapper.java
@@ -1,8 +1,6 @@
 package com.ebentos.backend.repository;
 
-import com.ebentos.backend.model.Productora;
 import com.ebentos.backend.model.Usuario;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
--- backend/src/main/java/com/ebentos/backend/service/DepartamentoService.java
+++ backend/src/main/java/com/ebentos/backend/service/DepartamentoService.java
@@ -1,7 +1,6 @@
 package com.ebentos.backend.service;
 
 import com.ebentos.backend.model.Departamento;
-import com.ebentos.backend.model.Productora;
 import com.ebentos.backend.repository.DepartamentoMapper;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,12 +9,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class DepartamentoService {
     private final DepartamentoMapper departamentoMapper;
-    
+
     @Autowired
     public DepartamentoService(DepartamentoMapper departamentoMapper) {
         this.departamentoMapper = departamentoMapper;
     }
-    
+
     public List<Departamento> listarTodas() {
         return departamentoMapper.findAll();
     }
