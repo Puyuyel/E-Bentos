912437687e5a55e63d19cf8583666ab82ba3f5c1
marioSaico <mssopro2005@gmail.com>
2025-10-30 01:14:01 -0500

    Documentación lista de API's y Local CRUD completo

--- backend/src/main/java/com/ebentos/backend/controller/LocalController.java
+++ backend/src/main/java/com/ebentos/backend/controller/LocalController.java
@@ -2,11 +2,9 @@ package com.ebentos.backend.controller;
 
 import com.ebentos.backend.dto.LocalActualizaDTO;
 import com.ebentos.backend.dto.LocalDTO;
-import com.ebentos.backend.model.Local;
 import com.ebentos.backend.service.LocalService;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
