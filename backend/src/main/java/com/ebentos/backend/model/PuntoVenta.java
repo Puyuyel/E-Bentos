55b2536a9f0130dc0353c923e2e4c9692939b980
DELL <DELL@DESKTOP-FJE0BMO>
2025-11-02 00:26:00 -0500

    PuntoVenta paginado

--- backend/src/main/java/com/ebentos/backend/model/PuntoVenta.java
+++ backend/src/main/java/com/ebentos/backend/model/PuntoVenta.java
@@ -2,16 +2,22 @@ package com.ebentos.backend.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.*;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
 @Entity
 @Table(name = "PUNTOVENTA")
 @SQLDelete(sql = "UPDATE puntoventa SET activo = 0 WHERE puntoventa_id = ?")
@@ -33,59 +39,7 @@ public class PuntoVenta {
     @JoinColumn(name = "DISTRITO_ID", nullable = false)
     private Distrito distrito;
     
-    @Column(name = "ACTIVO", nullable = false)
-    private boolean activo = true;
-
-    public PuntoVenta(Integer puntoventaId, String nombre, String direccion, Distrito distrito) {
-        this.puntoventaId = puntoventaId;
-        this.nombre = nombre;
-        this.direccion = direccion;
-        this.distrito = distrito;
-    }
-
-    public PuntoVenta(){
-        
-    }
-
-    public Integer getPuntoventaId() {
-        return puntoventaId;
-    }
-
-    public void setPuntoventaId(Integer puntoventaId) {
-        this.puntoventaId = puntoventaId;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public Distrito getDistrito() {
-        return distrito;
-    }
-
-    public void setDistrito(Distrito distrito) {
-        this.distrito = distrito;
-    }
-    
-    public boolean isActivo() {
-        return activo;
-    }
-
-    public void setActivo(boolean activo) {
-        this.activo = activo;
-    }
-    
+    @Column(name = "ACTIVO", nullable = false, columnDefinition = "TINYINT")
+    private Integer activo;
     
 }
